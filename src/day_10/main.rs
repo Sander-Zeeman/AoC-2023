3577e19df0bdc399622c22e0f6c65ba408e6873c
Sander Zeeman <szeeman2000@gmail.com>
2023-12-11 12:40:51 +0100

    Day 11 complete

--- src/day_10/main.rs
+++ src/day_10/main.rs
@@ -1,16 +1,113 @@
 use common::read_input;
+use std::collections::VecDeque;
 
-fn part1(_input: &str) -> i32 {
-  return 0;
+#[derive(PartialEq, Copy, Clone)]
+enum Dir {
+  North,
+  East,
+  South,
+  West,
+  Start
 }
 
-fn part2(_input: &str) -> i32 {
+#[derive(PartialEq, Copy, Clone)]
+struct Pipe(Dir, Dir, bool);
+
+fn connect(pipe: Option<Pipe>, dir: Dir) -> bool {
+  if pipe.is_none() {
+    return false;
+  }
+
+  let pipe = pipe.unwrap();
+  let goal = match dir {
+    Dir::North => Dir::South,
+    Dir::East => Dir::West,
+    Dir::South => Dir::North,
+    Dir::West => Dir::East,
+    _ => panic!()
+  };
+  pipe.0 == goal || pipe.1 == goal
+}
+
+fn part1(input: &str) -> i32 {
+  let mut grid: Vec<Vec<Option<Pipe>>> = Vec::new();
+  let mut start_x = 0;
+  let mut start_y = 0;
+  for (i, line) in input.lines().enumerate() {
+    grid.push(Vec::new());
+    for (j, c) in line.chars().enumerate() {
+      let last_entry: &mut Vec<Option<Pipe>> = &mut grid[i];
+      last_entry.push(match c {
+        '|' => Some(Pipe(Dir::North, Dir::South, false)),
+        '-' => Some(Pipe(Dir::West, Dir::East, false)),
+        'L' => Some(Pipe(Dir::North, Dir::East, false)),
+        'J' => Some(Pipe(Dir::North, Dir::West, false)),
+        '7' => Some(Pipe(Dir::West, Dir::South, false)),
+        'F' => Some(Pipe(Dir::East, Dir::South, false)),
+        'S' => {
+          start_x = j;
+          start_y = i;
+          Some(Pipe(Dir::Start, Dir::Start, false))
+        },
+        '.' => None,
+        _ => panic!()
+      });
+    }
+  }
+
+  let mut conns: Vec<Dir> = Vec::new();
+  let y_len = grid.len();
+  let x_len = grid[0].len();
+
+  if start_y > 0 && connect(grid[start_y-1][start_x], Dir::North) {
+      conns.push(Dir::North);
+  }
+  if start_x > 0 && connect(grid[start_y][start_x-1], Dir::West) {
+    conns.push(Dir::West);
+  }
+  if start_y < y_len-1 && connect(grid[start_y+1][start_x], Dir::South) {
+    conns.push(Dir::South);
+  }
+  if start_x < x_len-1 && connect(grid[start_y][start_x+1], Dir::East) {
+    conns.push(Dir::East);
+  }
+
+  grid[start_y][start_x] = Some(Pipe(conns[0].clone(), conns[1].clone(), false));
+
+  let mut queue: VecDeque<(usize, usize)> = VecDeque::new();
+  queue.push_back((start_y, start_x));
+
+  while let Some((y, x)) = queue.pop_front() {
+    let pipe = grid[y][x].unwrap();
+    if pipe.2 {
+      continue;
+    }
+
+    grid[y][x] = Some(Pipe(pipe.0, pipe.1, true));
+    if y > 0 && connect(grid[y-1][x], Dir::North) {
+      queue.push_back((y-1, x));
+    }
+    if x > 0 && connect(grid[y][x-1], Dir::West) {
+      queue.push_back((y, x-1));
+    }
+    if y < y_len-1 && connect(grid[y+1][x], Dir::South) {
+      queue.push_back((y+1, x));
+    }
+    if x < x_len-1 && connect(grid[y][x+1], Dir::East) {
+      queue.push_back((y, x+1));
+    }
+  }
+
+  0
+}
+
+fn _part2(_input: &str) -> i32 {
   return 0;
 }
 
 fn main() {
   println!("Test 1: {}", part1(read_input("./test.txt").as_str()));
-  println!("Test 2: {}", part2(read_input("./test.txt").as_str()));
-  println!("Part 1: {}", part1(read_input("./input1.txt").as_str()));
-  println!("Part 2: {}", part2(read_input("./input2.txt").as_str()));
+  // println!("Test 2: {}", part2(read_input("./test.txt").as_str()));
+  // println!("Part 1: {}", part1(read_input("./input.txt").as_str()));
+  // println!("Part 2: {}", part2(read_input("./input.txt").as_str()));
 }
--- src/day_11/main.rs
+++ src/day_11/main.rs
@@ -1,16 +1,145 @@
 use common::read_input;
 
-fn part1(_input: &str) -> i32 {
-  return 0;
+fn transpose<T>(original: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    assert!(!original.is_empty());
+    let mut transposed = (0..original[0].len()).map(|_| vec![]).collect::<Vec<_>>();
+
+    for original_row in original {
+        for (item, transposed_row) in original_row.into_iter().zip(&mut transposed) {
+            transposed_row.push(item);
+        }
+    }
+
+    transposed
 }
 
-fn part2(_input: &str) -> i32 {
-  return 0;
+fn part1(input: &str) -> i32 {
+    let universe: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    let mut galaxies: Vec<(i32, i32)> = Vec::new();
+    for (i, row) in universe.iter().enumerate() {
+        for (j, c) in row.iter().enumerate() {
+            if *c == '#' {
+                galaxies.push((i as i32, j as i32));
+            }
+        }
+    }
+
+    let mut x_empty = Vec::new();
+    let mut y_empty = Vec::new();
+
+    for (i, row) in universe.iter().enumerate() {
+        let size = row.iter().filter(|c| **c == '#').count();
+        if size == 0 {
+            y_empty.push(i);
+        }
+    }
+
+    for (i, row) in transpose::<char>(universe).iter().enumerate() {
+        let size = row.iter().filter(|c| **c == '#').count();
+        if size == 0 {
+            x_empty.push(i);
+        }
+    }
+
+    for galaxy in &mut galaxies {
+        let mut x_total = 0;
+        let mut y_total = 0;
+
+        for x in x_empty.iter() {
+            let x = *x as i32;
+            if galaxy.1 > x {
+                x_total += 1;
+            }
+        }
+        for y in y_empty.iter() {
+            let y = *y as i32;
+            if galaxy.0 > y {
+                y_total += 1;
+            }
+        }
+
+        galaxy.0 += y_total;
+        galaxy.1 += x_total;
+    }
+
+    let mut total = 0;
+    for i in 0..galaxies.len() {
+        for j in i + 1..galaxies.len() {
+            let g1 = galaxies[i];
+            let g2 = galaxies[j];
+            let dist = (g1.0 - g2.0).abs() + (g1.1 - g2.1).abs();
+            total += dist;
+        }
+    }
+
+    total
+}
+
+fn part2(input: &str) -> isize {
+    let universe: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    let mut galaxies: Vec<(isize, isize)> = Vec::new();
+    for (i, row) in universe.iter().enumerate() {
+        for (j, c) in row.iter().enumerate() {
+            if *c == '#' {
+                galaxies.push((i as isize, j as isize));
+            }
+        }
+    }
+
+    let mut x_empty = Vec::new();
+    let mut y_empty = Vec::new();
+
+    for (i, row) in universe.iter().enumerate() {
+        let size = row.iter().filter(|c| **c == '#').count();
+        if size == 0 {
+            y_empty.push(i);
+        }
+    }
+
+    for (i, row) in transpose::<char>(universe).iter().enumerate() {
+        let size = row.iter().filter(|c| **c == '#').count();
+        if size == 0 {
+            x_empty.push(i);
+        }
+    }
+
+    for galaxy in &mut galaxies {
+        let mut x_total = 0;
+        let mut y_total = 0;
+
+        for x in x_empty.iter() {
+            let x = *x as isize;
+            if galaxy.1 > x {
+                x_total += 999999;
+            }
+        }
+        for y in y_empty.iter() {
+            let y = *y as isize;
+            if galaxy.0 > y {
+                y_total += 999999;
+            }
+        }
+
+        galaxy.0 += y_total;
+        galaxy.1 += x_total;
+    }
+
+    let mut total = 0;
+    for i in 0..galaxies.len() {
+        for j in i + 1..galaxies.len() {
+            let g1 = galaxies[i];
+            let g2 = galaxies[j];
+            let dist = (g1.0 - g2.0).abs() + (g1.1 - g2.1).abs();
+            total += dist;
+        }
+    }
+
+    total
 }
 
 fn main() {
-  println!("Test 1: {}", part1(read_input("./test.txt").as_str()));
-  println!("Test 2: {}", part2(read_input("./test.txt").as_str()));
-  println!("Part 1: {}", part1(read_input("./input1.txt").as_str()));
-  println!("Part 2: {}", part2(read_input("./input2.txt").as_str()));
+    println!("Test 1: {}", part1(read_input("./test.txt").as_str()));
+    println!("Test 2: {}", part2(read_input("./test.txt").as_str()));
+    println!("Part 1: {}", part1(read_input("./input.txt").as_str()));
+    println!("Part 2: {}", part2(read_input("./input.txt").as_str()));
 }
