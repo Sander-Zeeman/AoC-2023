b4f7cf6fbf5ecf81c378251bce9b44d7a9db5ba4
Sander Zeeman <szeeman2000@gmail.com>
2023-12-16 00:20:31 +0100

    Day 15 complete, part 2 is just busywork, I'll get back to this later

--- src/day_15/main.rs
+++ src/day_15/main.rs
@@ -1,16 +1,33 @@
 use common::read_input;
 
-fn part1(_input: &str) -> i32 {
-  return 0;
+fn hash(s: &str) -> usize {
+    let bs = s.as_bytes();
+    let mut curr = 0;
+    for b in bs {
+        curr += *b as usize;
+        curr *= 17;
+        curr %= 256;
+    }
+    curr
 }
 
-fn part2(_input: &str) -> i32 {
-  return 0;
+fn part1(input: &str) -> i32 {
+    input
+        .lines()
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|code| hash(code))
+        .sum::<usize>() as i32
+}
+
+fn _part2(_input: &str) -> i32 {
+    return 0;
 }
 
 fn main() {
-  println!("Test 1: {}", part1(read_input("./test.txt").as_str()));
-  println!("Test 2: {}", part2(read_input("./test.txt").as_str()));
-  println!("Part 1: {}", part1(read_input("./input1.txt").as_str()));
-  println!("Part 2: {}", part2(read_input("./input2.txt").as_str()));
+    println!("Test 1: {}", part1(read_input("./test.txt").as_str()));
+    // println!("Test 2: {}", part2(read_input("./test.txt").as_str()));
+    println!("Part 1: {}", part1(read_input("./input.txt").as_str()));
+    // println!("Part 2: {}", part2(read_input("./input.txt").as_str()));
 }
